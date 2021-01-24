1ee47b7639a5deb2b22aa893f43189607a3791b5
Myles O'Connor <myles.oconnor@gmail.com>
2021-01-24 13:54:51 -0800

    Test ref with components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
+import { useState, useRef } from 'react'
 import Head from 'next/head'
 import { Box, Text } from 'component-library-tsdx-example'
 
 export default function Home() {
+  const [value, setValue] = useState('Next.js!')
+  const inputRef = useRef()
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setValue(e.target.value)
+  }
+
   return (
     <>
       <Head>
@@ -12,8 +21,13 @@ export default function Home() {
       <Box as='main'>
         <Text as='h1'>
           Welcome to{' '}
-          <Text as='a' color='primary' href='https://nextjs.org' display='inline-block'>
-            Next.js!
+          <Text
+            as='a'
+            color='primary'
+            href='https://nextjs.org'
+            display='inline-block'
+          >
+            {value}
           </Text>
         </Text>
 
@@ -21,15 +35,29 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </Text>
 
+        <Box mb={5}>
+          <Box
+            as='input'
+            type='text'
+            ref={inputRef}
+            defaultValue={value}
+            onChange={handleChange}
+          />
+        </Box>
+
         <Box>
           <Text as='a' color='primary' href='https://nextjs.org/docs' mb={5}>
             <Text as='h3'>Documentation &rarr;</Text>
-            <Text>Find in-depth information about Next.js features and API.</Text>
+            <Text>
+              Find in-depth information about Next.js features and API.
+            </Text>
           </Text>
 
           <Text as='a' color='primary' href='https://nextjs.org/learn' mb={5}>
             <Text as='h3'>Learn &rarr;</Text>
-            <Text>Learn about Next.js in an interactive course with quizzes!</Text>
+            <Text>
+              Learn about Next.js in an interactive course with quizzes!
+            </Text>
           </Text>
 
           <Text
@@ -39,7 +67,9 @@ export default function Home() {
             mb={5}
           >
             <Text as='h3'>Examples &rarr;</Text>
-            <Text>Discover and deploy boilerplate example Next.js projects.</Text>
+            <Text>
+              Discover and deploy boilerplate example Next.js projects.
+            </Text>
           </Text>
 
           <Text
@@ -49,7 +79,9 @@ export default function Home() {
             mb={5}
           >
             <Text as='h3'>Deploy &rarr;</Text>
-            <Text>Instantly deploy your Next.js site to a public URL with Vercel.</Text>
+            <Text>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </Text>
           </Text>
         </Box>
       </Box>
