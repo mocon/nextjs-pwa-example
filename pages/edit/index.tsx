5e04200e915e18250ceaf2573bb96bd1968dcf6e
Myles O'Connor <myles.oconnor@gmail.com>
2021-01-24 20:15:53 -0800

    Fixed exports for new and edit

--- pages/edit/index.tsx
+++ pages/edit/index.tsx
@@ -1,1 +1,10 @@
-export {}
+import { Text } from 'component-library-tsdx-example'
+import { Container } from '../../src/components'
+
+export default function EditSymbol() {
+  return (
+    <Container>
+      <Text>Edit symbol page</Text>
+    </Container>
+  )
+}
--- pages/new/index.tsx
+++ pages/new/index.tsx
@@ -1,1 +1,10 @@
-export {}
+import { Text } from 'component-library-tsdx-example'
+import { Container } from '../../src/components'
+
+export default function NewSymbol() {
+  return (
+    <Container>
+      <Text>New symbol page</Text>
+    </Container>
+  )
+}
