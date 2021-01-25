bfe583acaa8d2ccfd408478d176974f213bab365
Myles O'Connor <myles.oconnor@gmail.com>
2021-01-24 20:09:46 -0800

    Temporary fix for Container styles

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -5,8 +5,14 @@ type ContainerProps = {
   children?: ReactNode
 }
 
+// TODO: Update to <Box className='Container' mx='auto' maxWidth={['100%', 800]} px={3}>
+// after fixing component library styled-system exports, see https://codesandbox.io/s/04w296kk6p
+
 export const Container: FC<ContainerProps> = ({ children }) => (
-  <Box className='Container' mx='auto' maxWidth={['100%', 800]} px={3}>
+  <div
+    className='Container'
+    style={{ margin: '0 auto', padding: '0 16px', maxWidth: 800 }}
+  >
     {children}
-  </Box>
+  </div>
 )
