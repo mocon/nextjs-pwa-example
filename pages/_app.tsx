85c46d386255c0b5f0e6baa208e25a1cac4006e3
Myles O'Connor <myles.oconnor@gmail.com>
2021-01-26 09:36:27 -0800

    Updated status bar style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,7 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta name='application-name' content='Next App' />
         <meta name='apple-mobile-web-app-capable' content='yes' />
-        <meta
-          name='apple-mobile-web-app-status-bar-style'
-          content='black-translucent'
-        />
+        <meta name='apple-mobile-web-app-status-bar-style' content='black' />
         <meta name='apple-mobile-web-app-title' content='Next App' />
         <meta name='description' content='Best PWA App in the world' />
         <meta name='format-detection' content='telephone=no' />
