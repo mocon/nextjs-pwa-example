78e9af53c7610f659aa4499d3d1f975bf2dcc419
Myles O'Connor <myles.oconnor@gmail.com>
2021-01-26 20:36:29 -0800

    Share button only on home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import wretch from 'wretch'
+import { Box } from 'component-library-tsdx-example'
 import {
   Button,
   Container,
@@ -24,6 +25,16 @@ export default function HomeScreen({ cryptoData }) {
   async function getSymbolsFromLocalStorage() {
     const localSymbols = await JSON.parse(localStorage.getItem('symbols'))
     if (localSymbols) setSymbols(localSymbols)
+  }
+
+  const shareUrl = async () => {
+    try {
+      await navigator.share({
+        url: window.location.href,
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   useEffect(() => {
@@ -54,6 +65,18 @@ export default function HomeScreen({ cryptoData }) {
         </List>
 
         {symbols.length === 0 && <Empty message='No symbols tracked' />}
+
+        {/* Share button */}
+        <Box
+          className='Share'
+          position='fixed'
+          left={0}
+          bottom={0}
+          right={0}
+          p={3}
+        >
+          <Button onClick={shareUrl}>Share</Button>
+        </Box>
       </Container>
     </>
   )
--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,40 +1,15 @@
 import { Box } from 'component-library-tsdx-example'
-import { Button } from '../Button'
 
-export const Container = ({ children }) => {
-  const shareUrl = async () => {
-    try {
-      await navigator.share({
-        url: window.location.href,
-      })
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  return (
-    <>
-      <Box
-        className='Container'
-        display='flex'
-        flexDirection='column'
-        flex={1}
-        overflowY='auto'
-        px={3}
-        backgroundColor='background'
-      >
-        {children}
-      </Box>
-      <Box
-        className='Share'
-        position='fixed'
-        left={0}
-        bottom={0}
-        right={0}
-        p={3}
-      >
-        <Button onClick={shareUrl}>Share</Button>
-      </Box>
-    </>
-  )
-}
+export const Container = ({ children }) => (
+  <Box
+    className='Container'
+    display='flex'
+    flexDirection='column'
+    flex={1}
+    overflowY='auto'
+    px={3}
+    backgroundColor='background'
+  >
+    {children}
+  </Box>
+)
